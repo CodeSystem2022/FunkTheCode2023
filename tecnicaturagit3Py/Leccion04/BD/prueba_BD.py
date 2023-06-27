339a4b15251136e78c617468fa293b924917920a
Andrés Montes <88084994+Andy94m@users.noreply.github.com>
2023-06-27 18:57:29 -0300

    Update prueba_BD.py
    agrego codigo a mi rama

--- tecnicaturagit3Py/Leccion04/BD/prueba_BD.py
+++ tecnicaturagit3Py/Leccion04/BD/prueba_BD.py
@@ -1,4 +1,4 @@
-import psycopg2 # esto es para poder conectarnos a postgre
+import psycopg2 # conectamos postgre
 
 conexion = psycopg2.connect(
     user = 'postgres',
@@ -7,18 +7,18 @@ conexion = psycopg2.connect(
     port='5432',
     database='test_bd'
 )
+
 try:
     with conexion:
         with conexion.cursor() as cursor:
             sentencia = 'SELECT * FROM persona WHERE id_persona = %s'
             id_persona = input("digite un numero para el id_persona: ")
-            cursor.execute(sentencia,(id_persona,)) # de esta manera ejecutamos la sentencia
+            cursor.execute(sentencia,(id_persona,)) # ejecutamos sentencia
             registros = cursor.fetchone() # recuperamos todos los registros que seran una lista
             print(registros)
+
 
 except Exception as e:
     print(f'Ocurrio un error: {e}')
 finally:
     conexion.close()
-
-# https_//www.psycopg.org/focs/usage.html
