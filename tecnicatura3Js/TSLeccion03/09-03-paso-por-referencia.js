f191e4d235a67cfaf1d98c9f2dec8da524a89821
Andrés Montes <88084994+Andy94m@users.noreply.github.com>
2023-06-27 00:35:07 -0300

    Update 09-03-paso-por-referencia.js
    Agrego mi commit al main, paso por referencia, andres montes

--- tecnicatura3Js/TSLeccion03/09-03-paso-por-referencia.js
+++ tecnicatura3Js/TSLeccion03/09-03-paso-por-referencia.js
@@ -1,4 +1,4 @@
-/** PASO POR REFERENCIA **/
+/** PASO POR REFERENCIA 
 // la constante persona guarda la referencia a un objeto
 const persona = {
     nombre: "Juan",
@@ -17,4 +17,20 @@ const persona = {
   console.group(persona)
   // OUTPUT:
   // persona luego de cambiarObjeto: 
-  // { nombre: 'Ignacio', apellido: 'Perez' }
+  // { nombre: 'Ignacio', apellido: 'Perez' } **/
+
+const persona = {
+    nombre: "Juan",
+    apellido: "Lopez"
+  }
+  console.log(persona)
+  // OUTPUT: { nombre: 'Juan', apellido: 'Lopez' }
+  
+  function cambiarObjeto(p1) {
+    p1.nombre = 'Ignacio'
+    p1.apellido = 'Perez'
+  }
+  
+  cambiarObjeto(persona)
+  console.log("persona luego de cambiarObjeto: ")
+  console.group(persona)
