86f8b845f06297d9fa59b5e49ee75430b69ea589
Bárbara Herrera <114678422+Lenovitch@users.noreply.github.com>
2023-06-27 01:18:35 -0300

    Update Clase4.java
    Semana 3 - Modificador de acceso private

--- Java/Leccion03/ModificadoresAcceso/src/paquete2/Clase4.java
+++ Java/Leccion03/ModificadoresAcceso/src/paquete2/Clase4.java
@@ -21,6 +21,7 @@ public class Clase4 {
     public String getAtributoPrivate() {
         return atributoPrivate;
     }
+    */
 
     public void setAtributoPrivate(String atributoPrivate) {
         this.atributoPrivate = atributoPrivate;
@@ -28,3 +29,36 @@ public class Clase4 {
     
     
 }
+
+/*
+package paquete2;
+
+public class Clase4 {
+    private String atributoPrivate = "atributo Privado";
+    
+    private Clase4(){
+        System.out.println("Constructor privado");
+    }
+    
+    //creamos un constructor public para poder crear objetos
+    public Clase4(String argumento){ // aqui se puede llamar al constructor vacio
+        this();
+        System.out.println("Constructor publico");
+    }
+    //metodo private
+    private void metodoPrivado(){
+            System.out.println("Metodo Privado");
+    }
+
+    public String getAtributoPrivate() {
+        return atributoPrivate;
+    }
+    */
+
+    public void setAtributoPrivate(String atributoPrivate) {
+        this.atributoPrivate = atributoPrivate;
+    }
+    
+    
+}
+*/
