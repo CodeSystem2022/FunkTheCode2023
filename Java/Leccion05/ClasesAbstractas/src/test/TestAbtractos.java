16adf8d2a61f664a979f85c26a6c8f08d5e8814f
D15ER <113564080+D15ER@users.noreply.github.com>
2023-06-27 22:41:17 -0300

    TestAbtractos.java
    ads

--- Java/Leccion05/ClasesAbstractas/src/test/TestAbtractos.java
+++ Java/Leccion05/ClasesAbstractas/src/test/TestAbtractos.java
@@ -9,3 +9,15 @@ public class TestAbtractos {
         figura.dibujar();
     }
 }
+/*Codigo de Matias DIaz de Otazu
+package test;
+
+import domain.*;
+
+public class TestAbtractos {
+    public static void main(String[] args) {
+        FiguraGeometrica figura = new Rectangulo("Rectangulo");
+        figura.dibujar();
+    }
+}
+*/
