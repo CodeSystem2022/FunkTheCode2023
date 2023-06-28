0f9819c5b1e596c3a93bfd04ce7bc21b730f3be9
D15ER <113564080+D15ER@users.noreply.github.com>
2023-06-27 22:40:46 -0300

    Rectangulo.java
    asdasd

--- Java/Leccion05/ClasesAbstractas/src/domain/Rectangulo.java
+++ Java/Leccion05/ClasesAbstractas/src/domain/Rectangulo.java
@@ -12,3 +12,19 @@ public class Rectangulo extends FiguraGeometrica {
         System.out.println("se imprime un : "+ this.getClass().getSimpleName());
     }
 }
+//Codigo de Matias Diaz de Otazu
+/*
+package domain;
+
+public class Rectangulo extends FiguraGeometrica {
+    //constructor
+    public Rectangulo (String tipoFigura){
+        super(tipoFigura);
+    }
+    
+    @Override
+    public void dibujar(){// implementando el metodo
+        System.out.println("se imprime un : "+ this.getClass().getSimpleName());
+    }
+}
+*/
