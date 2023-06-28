390864fd6aa40f9c0f641ce81bec281800380ae9
NicoRizoA <112594572+NicoRizoA@users.noreply.github.com>
2023-06-28 00:05:51 -0300

    Update Computadora.java

--- Java/Leccion01/MundoPc/src/ar/com/system2023/mundopc/Computadora.java
+++ Java/Leccion01/MundoPc/src/ar/com/system2023/mundopc/Computadora.java
@@ -1,6 +1,74 @@
+// package ar.com.system2023.mundopc;
+
+// public class Computadora {
+//    private final int idComputadora;
+//    private String nombre;
+//    private Monitor monitor;
+//    private Teclado teclado;
+//    private Raton raton;
+//    private static int contadorComputadoras;
+   
+//    //constructor vacio
+//    private Computadora(){
+//        this.idComputadora = ++Computadora.contadorComputadoras;
+//    }
+   
+//    //constructor 2
+//    public Computadora(String nombre, Monitor monitor, Teclado teclado, Raton raton){
+//        this();
+//        this.nombre = nombre;
+//        this.monitor = monitor;
+//        this.teclado = teclado;
+//        this.raton = raton;
+//    }
+//    public int getIdComputadora(){
+//        return idComputadora;
+// }
+//     public String getNombre() {
+//         return nombre;
+//     }
+
+//     public void setNombre(String nombre) {
+//         this.nombre = nombre;
+//     }
+
+//     public Monitor getMonitor() {
+//         return monitor;
+//     }
+
+//     public void setMonitor(Monitor monitor) {
+//         this.monitor = monitor;
+//     }
+
+//     public Teclado getTeclado() {
+//         return teclado;
+//     }
+
+//     public void setTeclado(Teclado teclado) {
+//         this.teclado = teclado;
+//     }
+
+//     public Raton getRaton() {
+//         return raton;
+//     }
+
+//     public void setRaton(Raton raton) {
+//         this.raton = raton;
+//     }
+
+//     @Override
+//     public String toString() {
+//         return "Computadora{" + "idComputadora=" + idComputadora + ", nombre=" + nombre + ", monitor=" + monitor + ", teclado=" + teclado + ", raton=" + raton + '}';
+//     }
+   
+
+        
+// }
+
 package ar.com.system2023.mundopc;
 
-public class Computadora {
+public class Computadora 
+   {
    private final int idComputadora;
    private String nombre;
    private Monitor monitor;
@@ -8,58 +76,72 @@ public class Computadora {
    private Raton raton;
    private static int contadorComputadoras;
    
-   //constructor vacio
-   private Computadora(){
+   private Computadora()
+   {
        this.idComputadora = ++Computadora.contadorComputadoras;
    }
    
-   //constructor 2
-   public Computadora(String nombre, Monitor monitor, Teclado teclado, Raton raton){
+   public Computadora(String nombre, Monitor monitor, Teclado teclado, Raton raton)
+      {
        this();
        this.nombre = nombre;
        this.monitor = monitor;
        this.teclado = teclado;
        this.raton = raton;
    }
-   public int getIdComputadora(){
+      
+   public int getIdComputadora()
+      {
        return idComputadora;
-}
-    public String getNombre() {
+      }
+      
+    public String getNombre() 
+      {
         return nombre;
-    }
+      }
 
-    public void setNombre(String nombre) {
+    public void setNombre(String nombre) 
+    {
         this.nombre = nombre;
     }
 
-    public Monitor getMonitor() {
+    public Monitor getMonitor() 
+    {
         return monitor;
     }
 
-    public void setMonitor(Monitor monitor) {
+    public void setMonitor(Monitor monitor) 
+    {
         this.monitor = monitor;
     }
 
-    public Teclado getTeclado() {
+    public Teclado getTeclado() 
+    {
         return teclado;
     }
 
-    public void setTeclado(Teclado teclado) {
+    public void setTeclado(Teclado teclado) 
+    {
         this.teclado = teclado;
     }
 
-    public Raton getRaton() {
+    public Raton getRaton() 
+    {
         return raton;
     }
 
-    public void setRaton(Raton raton) {
+    public void setRaton(Raton raton) 
+    {
         this.raton = raton;
     }
 
     @Override
-    public String toString() {
+    public String toString() 
+    {
         return "Computadora{" + "idComputadora=" + idComputadora + ", nombre=" + nombre + ", monitor=" + monitor + ", teclado=" + teclado + ", raton=" + raton + '}';
     }
 
         
 }
+
+// Nicolás
