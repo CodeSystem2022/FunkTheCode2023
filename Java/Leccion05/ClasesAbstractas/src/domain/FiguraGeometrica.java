404c846d416a18b51313292ce8f3a45360da55a5
D15ER <113564080+D15ER@users.noreply.github.com>
2023-06-27 22:40:18 -0300

    FiguraGeometrica.java
    asd

--- Java/Leccion05/ClasesAbstractas/src/domain/FiguraGeometrica.java
+++ Java/Leccion05/ClasesAbstractas/src/domain/FiguraGeometrica.java
@@ -26,3 +26,33 @@ public abstract class FiguraGeometrica {
         return"FiguraGeometrica(" + "tipoFigura="+ tipoFigura + ")";
     }
 }
+
+//Codigo de Matias Diaz de Otazu
+/*package domain;
+
+public abstract class FiguraGeometrica {
+    protected String tipoFigura;
+    
+    protected FiguraGeometrica(String tipoFigura){
+        this.tipoFigura = tipoFigura;
+    }
+    
+    //Metodo abstracto
+    public abstract void dibujar();
+    
+    //agragamos el get y set
+    
+    public String getTipoFigura(){
+        return tipoFigura;
+    }
+    
+    public void setTipoFigura(String tipoFigura){
+        this.tipoFigura = tipoFigura;
+    }
+    
+    @Override
+    public String toString(){
+        return"FiguraGeometrica(" + "tipoFigura="+ tipoFigura + ")";
+    }
+}
+*/
