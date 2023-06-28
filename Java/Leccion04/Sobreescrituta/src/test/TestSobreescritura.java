e0cf07eeb2565e1d53ba7bfcc5fafa63d2c3577d
Andrés Montes <88084994+Andy94m@users.noreply.github.com>
2023-06-27 22:28:51 -0300

    TestSobreescritura.java
    Polimorfismo - andres montes - semana 4 JAVA

--- Java/Leccion04/Sobreescrituta/src/test/TestSobreescritura.java
+++ Java/Leccion04/Sobreescrituta/src/test/TestSobreescritura.java
@@ -1,26 +1,3 @@
-/*
-package test;
-
-import domain.*;
-
-
-public class TestSobreescritura {
-    public static void main(String[] args) {
-       Empleado empleado1 = new Empleado("Juan", 10000);
-       imprimir(empleado1);
-       // System.out.println("empleado1 = " + empleado1.obtenerDetalles());
-        empleado1 = new Gerente("Jose", 5000, "Sistemas");
-        imprimir(empleado1);
-        //System.out.println("gerente1 = " + gerente1.obtenerDetalles());
-    }
-    
-    public static void imprimir(Empleado empleado){
-        String detalles = empleado.obtenerDetalles();
-        System.out.println("detalles = " + detalles);
-    }
-} */
-
-------------------------------------------------------
 //Codigo andres Montes
 
 package test;
@@ -43,4 +20,3 @@ public class TestSobreescritura {
         System.out.println("detalles = " + detalles);
     }
 }
-
