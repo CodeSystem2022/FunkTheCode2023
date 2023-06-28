c5ebd23d8dc7c5cdc7916913ae0ffc1434d557de
D15ER <113564080+D15ER@users.noreply.github.com>
2023-06-27 22:09:39 -0300

    02-03-arreglos.js
    ISsue #18

--- tecnicatura3Js/TSLeccion02/02-03-arreglos.js
+++ tecnicatura3Js/TSLeccion02/02-03-arreglos.js
@@ -20,7 +20,7 @@ for (let i = 0; i < autos.length; i++) {
 for (let i = 0; i < motos.length; i++) {
   console.log(i + "=" + motos, [i]);
 }
-
+// Codigo de Matias
 //para modificar un elemento de un array:
 
 autos[2] = "volvo";
@@ -46,3 +46,29 @@ console.log(Array.isArray(autos));
 console.log(Array.isArray(motos));
 //otra manera para preguntar si es un array:
 console.log(autos instanceof Array);
+/*//para modificar un elemento de un array:
+
+autos[2] = "volvo";
+console.log(autos[2]);
+console.log(autos);
+
+//agregar nuevos elementos:
+autos.push("Toyota");
+console.log(autos);
+
+//otra manera de agregar elementos;
+
+autos[autos.length] = "porsche";
+console.log(autos);
+
+//otra manera pero que hay que tener cuidado al emplearla: no es una buena practica.
+
+autos[8] = "mercedez benz";
+console.log(autos);
+
+//como preguntar si es un array:
+console.log(Array.isArray(autos));
+console.log(Array.isArray(motos));
+//otra manera para preguntar si es un array:
+console.log(autos instanceof Array);
+*/
