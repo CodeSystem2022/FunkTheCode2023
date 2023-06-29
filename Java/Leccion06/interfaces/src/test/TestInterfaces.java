d2d51574fe88df514d93c953d61ea72a5e72b7da
Matias Diaz de Otazu <113564080+D15ER@users.noreply.github.com>
2023-06-29 17:18:54 -0300

    TestInterfaces.java
    ads

--- Java/Leccion06/interfaces/src/test/TestInterfaces.java
+++ Java/Leccion06/interfaces/src/test/TestInterfaces.java
@@ -18,3 +18,24 @@ public class TestInterfaces {
         
     }
 }
+/*Codigo de Matias Diaz de Otazu
+package test;
+
+import accesodatos.*;
+
+
+public class TestInterfaces {
+    public static void main(String[] args) {
+        IAccesoDatos datos = new ImplementacionMySql();
+        //datos.listar();
+        //imprimir(datos);
+        datos = new ImplementacionOracle();
+        //datos.listar();
+        imprimir(datos);
+    }
+    
+    public static void imprimir(IAccesoDatos datos) {
+        datos.listar();
+        
+    }
+}*/
