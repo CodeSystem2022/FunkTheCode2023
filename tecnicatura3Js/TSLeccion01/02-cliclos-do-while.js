3e03566d486fd142adea47960c059164ceee1b60
Bárbara Herrera <114678422+Lenovitch@users.noreply.github.com>
2023-06-28 21:16:24 -0300

    02-cliclos-do-while.js
    Semana 1 - Ciclo Do While

--- tecnicatura3Js/TSLeccion01/02-cliclos-do-while.js
+++ tecnicatura3Js/TSLeccion01/02-cliclos-do-while.js
@@ -1,7 +1,16 @@
-//CICLO: DO WHILE 
+//CICLO DO WHILE - Bárbara
 let conteo = 0;
 do{
     console.log(conteo);
     conteo++;
 }while(conteo < 3);
 console.log("Fin del ciclo do while");
+
+/* CICLO: DO WHILE 
+let conteo = 0;
+do{
+    console.log(conteo);
+    conteo++;
+}while(conteo < 3);
+console.log("Fin del ciclo do while");
+*/
