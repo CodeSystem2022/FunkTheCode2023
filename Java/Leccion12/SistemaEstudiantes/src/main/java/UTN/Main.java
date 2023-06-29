510d96780f0b81153889194f2742c419f8ce3a07
Matias Diaz de Otazu <113564080+D15ER@users.noreply.github.com>
2023-06-29 17:36:56 -0300

    Main.java

--- Java/Leccion12/SistemaEstudiantes/src/main/java/UTN/Main.java
+++ Java/Leccion12/SistemaEstudiantes/src/main/java/UTN/Main.java
@@ -12,3 +12,18 @@ public class Main {
         }
     }
 }
+/*Codigo de Matias Diaz de Otazu
+package UTN;
+
+import UTN.conexion.Conexion;
+
+public class Main {
+    public static void main(String[] args) {
+        var conexion = Conexion.getConnection();
+        if(conexion != null){
+            System.out.println("Conexion exitosa: "+conexion);
+        }else {
+            System.out.println("Error al conectarse");
+        }
+    }
+}*/
