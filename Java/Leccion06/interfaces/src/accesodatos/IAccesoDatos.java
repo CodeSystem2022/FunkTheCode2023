dd16e6c192d35d62f9e4f7a54d1ff416f65bb8de
Matias Diaz de Otazu <113564080+D15ER@users.noreply.github.com>
2023-06-29 17:16:54 -0300

    IAccesoDatos.java
    asd

--- Java/Leccion06/interfaces/src/accesodatos/IAccesoDatos.java
+++ Java/Leccion06/interfaces/src/accesodatos/IAccesoDatos.java
@@ -14,3 +14,21 @@ public interface IAccesoDatos {
     
     void eliminar();
 }
+
+/*COdigo de Matias Diaz de Otazu
+package accesodatos;
+
+public interface IAccesoDatos {
+    int MAX_REGISTRO = 10 ;
+    //Metodo insertar es abstracto y sin cuerpo
+    
+    void insertar();
+    
+    
+    void listar();
+    
+    void actaulizar();
+    
+    void eliminar();
+}
+*/
