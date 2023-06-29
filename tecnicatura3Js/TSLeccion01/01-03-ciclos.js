d2011af2bd0d24f40badd535f94843440f349a02
Bárbara Herrera <114678422+Lenovitch@users.noreply.github.com>
2023-06-28 21:14:06 -0300

    01-03-ciclos.js
    Semana 1 - Ciclo While

--- tecnicatura3Js/TSLeccion01/01-03-ciclos.js
+++ tecnicatura3Js/TSLeccion01/01-03-ciclos.js
@@ -1,10 +1,20 @@
-//CICLO: WHILE
+
+//CICLO WHILE - Bárbara
 let contador = 0;
 while(contador < 3){
     console.log(contador);
     contador++;
 }
 console.log("Fin del ciclo while");
+
+/* CICLO: WHILE
+let contador = 0;
+while(contador < 3){
+    console.log(contador);
+    contador++;
+}
+console.log("Fin del ciclo while");
+*/
 
 /* wHILE 
 let contador = 0
