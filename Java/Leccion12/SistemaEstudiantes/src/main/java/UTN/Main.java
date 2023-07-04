3d8e714f88d58c554615b8133fd1e33044352ee5
NicoRizoA <112594572+NicoRizoA@users.noreply.github.com>
2023-07-04 19:20:19 -0300

    Update Main.java

--- Java/Leccion12/SistemaEstudiantes/src/main/java/UTN/Main.java
+++ Java/Leccion12/SistemaEstudiantes/src/main/java/UTN/Main.java
@@ -5,11 +5,12 @@ import UTN.conexion.Conexion;
 public class Main {
     public static void main(String[] args) {
         var conexion = Conexion.getConnection();
-        if(conexion != null){
-            System.out.println("Conexion exitosa: "+conexion);
-        }else {
-            System.out.println("Error al conectarse");
-        }
+    if(conexion != null) 
+    {
+        System.out.println("Conexion exitosa: " + conexion);
+    }
+    else
+             System.out.println("Error al conectarse");
     }
 }
 /*Codigo de Matias Diaz de Otazu
